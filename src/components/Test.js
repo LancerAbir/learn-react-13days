fd8e6bf46f8d15a29b8bc8a690e2759293cdfecf
LancerAbir <anjan.o24@gmail.com>
2022-07-30 10:52:57 +0600

    new

--- src/components/Test.js
+++ src/components/Test.js
@@ -7,8 +7,6 @@ const Test = ({propName, propSetName}) => {
             <h1> Ami {propName}</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto reprehenderit quidem tempore ratione quo debitis dicta quis ad eos fugit asperiores esse ducimus mollitia deserunt eveniet ab est commodi, possimus ipsa voluptates quam! Ipsum, qui nobis nemo sapiente rerum blanditiis accusantium tempore assumenda doloribus ullam, maxime minus veniam, harum vitae.</p>
             <button onClick={() => propSetName('Abir er Bou')}> Click Me</button>
-
-
         </>
     );
 };
